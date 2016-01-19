433639074812b80591885bf7858f9667f71b8c26
Andrei Visan <andrei.m.visan@gmail.com>
2016-01-19 12:26:12 +0100

    Added parsing for tasks.json

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs");
 
 var taskEvents = require('../task-events')
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', getTasks, function(req, res) {
+  res.render('index', { title: 'Task manager' , tasks: req.tasks});
 });
 
 router.get('/start-task', function(req, res) {
     taskEvents.emit('startTimer', res);
-})
+});
+
+function getTasks(req, res, next) {
+    var filePath = "../tasks.json";
+    var fileContent = fs.readFileSync(filePath);
+    req.tasks = fileContent;
+    next();
+}
 
 module.exports = router;
